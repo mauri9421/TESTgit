66a1efa0c2192bc6d42abb28ec2e57e74eada167
MauricioPerez <mauri942@gmail.com>
2021-01-28 16:46:05 -0500

    add clase prueba

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //para probar commits y push
+        //segunda prueba de escritura de codigo
+        //tercer comentario del codigo
     }
 }
